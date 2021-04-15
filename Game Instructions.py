a8f10ea5228370fad76b88bf77cd046899e24eb8
Fardin <khoque24@wabash.edu>
2021-04-15 12:05:17 -0400

    Game Instructions for Player

---
+++ Game Instructions.py
@@ -0,0 +1,29 @@
+class GameInstructions:
+    #Basic
+    print("This is the very Basic version of the game.This is for First Timers")
+    print("The Rules are the following")
+    print("a)There are 14 pegs that are placed in a triangle made of 15 holes")
+    print("b)One hole is open at the beginning of every game")
+    print("c)The goal of the game is to eliminate all but 1 peg from play")
+    print("d)Pegs can be elimated by jumping one peg over another)")
+    print("e)A jump consists of picking up one peg, moving over another peg, and placing it down in an empty hole.The jumped peg is then removed from the board.") 
+   
+    #Moderate
+    print("This is the moderate version of the game")
+    print("The Rules are the following")
+    print("a)There are 14 pegs that are placed in a triangle made of 15 holes")
+    print("b)One hole is open at the beginning of every game")
+    print("c)The goal of the game is to eliminate all but 1 peg from play")
+    print("d)Pegs can be elimated by jumping one peg over another)")
+    print("e)A jump consists of picking up one peg, moving over another peg, and placing it down in an empty hole.The jumped peg is then removed from the board.") 
+    #Advanced
+    print("This is the Advanced version of the game. Do not try unless you are familiar with the game")
+    print("The Rules are the following")
+    print("a)There are 20 pegs that are placed in a triangle made of 21 holes")
+    print("b)One hole is open at the beginning of every game")
+    print("c)The goal of the game is to eliminate all but 1 peg from play")
+    print("d)Pegs can be elimated by jumping one peg over another)")
+    print("e)A jump consists of picking up one peg, moving over another peg, and placing it down in an empty hole.The jumped peg is then removed from the board.") 
+
+
+
